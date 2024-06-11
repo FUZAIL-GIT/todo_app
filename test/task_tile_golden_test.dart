cc335b69dfa114dbfdd50d1004ddc25d0643a38d
FUZAIL-GIT <mohammadfuzailzaman@gmail.com>
2024-06-11 15:07:39 +0500

    code updated

--- test/task_tile_golden_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:todo_app/app.dart';
-
-void main() {
-  testWidgets('Golden test', (WidgetTester tester) async {
-    await tester.pumpWidget(const TodoApp());
-    await expectLater(find.byType(TodoApp), matchesGoldenFile('main.png'));
-  });
-}
